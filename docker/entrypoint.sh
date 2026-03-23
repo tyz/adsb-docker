d64ccffe975bcc27545d17d1907eba5ce36e9d96
Mathijs Maassen <m@hijs.net>
2026-03-23 20:25:06 +0100

    Move aircraft db

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+AIRCRAFT_DB=/data/aircraft.csv.gz
+UPINTHEAIR=/data/upintheair.json
+
 if [ "${ENABLE_BIAS_T}" = "true" ]; then
     echo "Activating Bias-T for active antenna..."
     /usr/local/bin/rtl_biast -b 1
@@ -7,16 +10,18 @@ if [ "${ENABLE_BIAS_T}" = "true" ]; then
     echo
 fi
 
-if [ "${HEYWHATSTHAT_ID}" -a ! -f /data/upintheair.json ]; then
+if [ "${HEYWHATSTHAT_ID}" -a ! -f ${UPINTHEAIR} ]; then
     echo "Creating upintheair.json for altitudes ${HEYWHATSTHAT_ID_ALTS-30000}"
-    curl -sLo /data/upintheair.json "http://www.heywhatsthat.com/api/upintheair.json?id=${HEYWHATSTHAT_ID}&refraction=0.25&alts=${HEYWHATSTHAT_ID_ALTS-30000}"
+    curl -sLo ${UPINTHEAIR} "http://www.heywhatsthat.com/api/upintheair.json?id=${HEYWHATSTHAT_ID}&refraction=0.25&alts=${HEYWHATSTHAT_ID_ALTS-30000}"
     echo
 fi
 
-echo "Updating aircraft database"
-curl -Lo /usr/local/share/tar1090/aircraft.csv.gz https://github.com/wiedehopf/tar1090-db/raw/csv/aircraft.csv.gz
-ls -lh /usr/local/share/tar1090/aircraft.csv.gz
-echo
+if [ ! -f ${AIRCRAFT_DB} ]; then
+    echo "Downloading aircraft database"
+    curl -Lo ${AIRCRAFT_DB} https://github.com/wiedehopf/tar1090-db/raw/csv/aircraft.csv.gz
+    ls -lh ${AIRCRAFT_DB}
+    echo
+fi
 
 echo "Starting readsb..."
 exec /app/readsb "$@"
